20296472b4e65edbec980fae950c1132c6a695ce
Luc Saunders <lucsaunders@gmail.com>
2019-07-20 17:22:06 -0400

    Clean up comments and commented-out code in users api route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -74,8 +74,10 @@ router.post(
 
       await user.save();
 
+      // Create payload object
       const payload = {
         user: {
+          // Mongoose abstracts Mongo's _id to .id
           id: user.id
         }
       };
@@ -83,7 +85,10 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 360000 },
+
+        // Set expiration to one hour
+        { expiresIn: 3600 },
+
         (error, token) => {
           if (error) throw error;
           response.json({ token });
@@ -97,102 +102,3 @@ router.post(
 );
 
 module.exports = router;
-
-//     User.findOne({ email: request.body.email }).then(user => {
-//       if (user) {
-//         errors.email = 'Email already exists';
-//         return response.status(400).json(errors);
-//       } else {
-//         const avatar = gravatar.url(request.body.email, {
-//           s: '200', // Size
-//           r: 'pg', // Rating
-//           d: 'mm' // Default
-//         });
-
-//         const newUser = new User({
-//           name: request.body.name,
-//           email: request.body.email,
-//           avatar,
-//           password: request.body.password
-//         });
-
-//         // Encrypt password (with 10 rounds)
-//         bcrypt.genSalt(10, (error, salt) => {
-//           bcrypt.hash(newUser.password, salt, (error, hash) => {
-//             if (error) throw error;
-//             newUser.password = hash;
-//             newUser
-//               .save()
-//               .then(user => response.json(user))
-//               .catch(error => console.log(error));
-//           });
-//         });
-//       }
-//     });
-//   }
-// );
-
-// // @route   GET api/users/login
-// // @desc    Login User / Returning JWT Token
-// // @access  Public
-// router.post('/login', (request, response) => {
-//   const { errors, isValid } = validationResult(request.body);
-
-//   // Check Validation
-//   if (!isValid) {
-//     return response.status(400).json(errors);
-//   }
-
-//   const email = request.body.email;
-//   const password = request.body.password;
-
-//   // Find user by email
-//   User.findOne({ email }).then(user => {
-//     // Check for user
-//     if (!user) {
-//       errors.email = 'User not found';
-//       return response.status(404).json(errors);
-//     }
-
-//     // Check Password
-//     bcrypt.compare(password, user.password).then(isMatch => {
-//       if (isMatch) {
-//         // User Matched
-//         const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
-
-//         // Sign Token
-//         jwt.sign(
-//           payload,
-//           keys.secretOrKey,
-//           { expiresIn: 3600 },
-//           (error, token) => {
-//             response.json({
-//               success: true,
-//               token: 'Bearer ' + token
-//             });
-//           }
-//         );
-//       } else {
-//         errors.password = 'Password incorrect';
-//         return response.status(400).json(errors);
-//       }
-//     });
-//   });
-// });
-
-// // @route   GET api/users/current
-// // @desc    Return current user
-// // @access  Private
-// router.get(
-//   '/current',
-//   passport.authenticate('jwt', { session: false }),
-//   (request, response) => {
-//     response.json({
-//       id: request.user.id,
-//       name: request.user.name,
-//       email: request.user.email
-//     });
-//   }
-// );
-
-// module.exports = router;
